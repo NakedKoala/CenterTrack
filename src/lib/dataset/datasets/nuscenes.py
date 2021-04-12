1f66d9ec2201d6bd456a32ef239a8dc62248ca13
NakedKoala <sirgarfield.c@gmail.com>
2021-04-12 14:15:38 -0400

    commit baseline state

--- src/lib/dataset/datasets/nuscenes.py
+++ src/lib/dataset/datasets/nuscenes.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 
 import pycocotools.coco as coco
 from pycocotools.cocoeval import COCOeval
-from pyquaternion import Quaternion
+# from pyquaternion import Quaternion
 import numpy as np
 import torch
 import json
